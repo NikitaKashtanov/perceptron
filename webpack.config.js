2c6012163d90e633ba4e17447104910032e597d9
Nikita Kastanov <nikita.kastanov@novakidschool.com>
2025-10-03 02:17:36 +0300

    Fix: Add CopyWebpackPlugin to copy models to dist/
    - Added copy-webpack-plugin dependency
    - Updated webpack.config.js to copy public/models/ to dist/models/
    - Now all model files are properly included in production build
    - Fixes issue where Vercel couldn't see static model files
    
    This ensures that all 6 pre-trained models are available in production.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -31,6 +32,15 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: 'index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public/models',
+          to: 'models',
+          noErrorOnMissing: true
+        }
+      ]
     })
   ],
   devServer: {
